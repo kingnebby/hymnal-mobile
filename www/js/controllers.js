cd769fd79770db93526a07a0b3abc58230245943
dvilla <daniel.a.villa@boeing.com>
2016-10-15 01:21:55 -0700

    Added app css, updated to use latest API.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,4 @@
 const api_base_url = 'http://hymnal-api.kingnebby.com/hymn'
-const api_search_path = '?keyword='
-const api_id_path = '/'
 
 angular.module('app.controllers', ['ionic'])
 
@@ -12,54 +10,103 @@ angular.module('app.controllers', ['ionic'])
     templateUrl: 'my-modal.html',
   }
 
+  // Used to display the info box.
   var poppy
-  $scope.showInfo = function () {
+  $scope.showInfo = function() {
     poppy = $ionicPopup.show(options)
   }
-  $scope.closeInfo = function () {
+  $scope.closeInfo = function() {
     poppy.close()
   }
 
   $scope.form = {
     search: undefined
   }
+
   // Got errors with this http://stackoverflow.com/questions/37367200/deferred-long-running-timer-tasks-to-improve-scrolling-smoothness
   const search_delay = 500
 
   var errHandler = function(res) {
-    console.error("Server Response: " + res.statusText + " Status Code: " + res.status)
+    if (res) {
+      if (res.statusText) {
+        console.error("Server Response: " + res.statusText + " Status Code: " + res.status)
+      } else {
+        console.error(res)
+      }
+    } else {
+      console.error(new Error('Unknown exception.'))
+    }
   }
 
   $scope.docs = []
 
-  $scope.search = function (e) {
-    console.log(e)
-  }
-
   var timeout
-  // var handleSearchChange
 
   $scope.$watch('form.search', handleSearchChange)
 
- function handleSearchChange (n, o, s) {
+  function handleSearchChange(n, o, s) {
+    $scope.searching = null
     clearTimeout(timeout)
-    // TODO: If number, search only on hymn numbers. !isNaN(Number(n)) -- should be server option.
-    // TODO: insert regexs between spaces to account for punctuation? -- should be server option.
-    // TODO: sort on number. - should be server default.
     if (n) {
       timeout = setTimeout(function() {
-        $http.get(api_base_url + api_search_path + n).then(function(res) {
-          if (res && res.data && res.data.results) {
-            var ret = res.data.results.sort(function(a,b) {
-              return Number(a.hymn_number) - Number(b.hymn_number)
-            })
-            ret = _.filter(ret, function(el) {
-              return el.hymn_number.match(n)
-            })
-            // console.log("Result count: " + res.data.results.length)
-            $scope.docs = res.data.results
+        $scope.searching = "Searching..."
+        $scope.loading = true
+
+        var url
+          // Check if number.
+        if (!isNaN(Number(n))) {
+          url = `${api_base_url}?hymn_number=${n}`
+        } else {
+          // Check title/first line.
+          url = `${api_base_url}?title=${n}&first_line=${n}&author=${n}`
+        }
+
+        // Search.
+        $http.get(url).then(function(res) {
+          // catch errors and pass data forward.
+          if (!res || !res.data) {
+            return Promise.reject(new Error("No data from server."))
           }
-        }, errHandler)
+          return res.data.results
+        }).then(function(results) {
+          // If results, just pass along.
+          if (results && results.length > 0) {
+            return results
+          }
+
+          // if no results, expand to search for phrase.
+          $scope.searching = "Looking deeper..."
+          url = `${api_base_url}?phrase=${n}`
+          return $http.get(url).then(function(res) {
+            if (!res || !res.data) {
+              return Promise.reject(new Error("No data from server."))
+            }
+            return res.data.results
+          })
+        }).then(function(results) {
+          if (results && results.length > 0) {
+            return results
+          }
+          $scope.searching = "One last try..."
+          url = `${api_base_url}?phrase=${n}&expanded=true`
+          return $http.get(url).then(function(res) {
+            if (!res || !res.data) {
+              return Promise.reject(new Error("No data from server."))
+            }
+            return res.data.results
+          })
+        }).then(function(results) {
+          $scope.loading = false
+          if (!results || results.length == 0) {
+            $scope.searching = "Sorry, no results found."
+            $scope.docs = []
+            return
+          }
+          $scope.searching = null
+
+          $scope.docs = results
+        }).catch(errHandler)
+
       }, search_delay)
     } else {
       $scope.docs = []
@@ -67,11 +114,20 @@ angular.module('app.controllers', ['ionic'])
   }
 })
 
+// Gets the specific hymn.
 .controller('hymnCtrl', function($scope, $stateParams, $http) {
   var errHandler = function(res) {
-    console.error("Server Response: " + res.statusText + " Status Code: " + res.status)
+    if (res) {
+      if (res.statusText) {
+        console.error("Server Response: " + res.statusText + " Status Code: " + res.status)
+      } else {
+        console.error(res)
+      }
+    } else {
+      console.error(new Error('Unknown exception.'))
+    }
   }
-  $http.get(api_base_url + api_id_path + $stateParams.id).then(function(res) {
-    $scope.hymn = res.data.results
+  $http.get(`${api_base_url}/${$stateParams.id}`).then(function(res) {
+    $scope.hymn = res.data
   }, errHandler)
 })
